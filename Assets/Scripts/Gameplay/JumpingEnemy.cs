a1ff622bef58d46acd40b26b70556d84dadf6549
Joaquín Labanda Castro <joaquinlabanda04@gmail.com>
2025-09-16 21:09:36 -0300

    [add] JumpingEnemy Script

---
+++ Assets/Scripts/Gameplay/JumpingEnemy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Platformer.Mechanics;
+
+public class JumpingEnemy : MonoBehaviour
+{
+    public AnimationController animController;
+    public double timer = 0;
+    public double initTimer = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initializeTimer(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            animController.jump = true;
+            initializeTimer();
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+            animController.jump = false;
+        }
+    }
+
+    void initializeTimer()
+    {
+        timer = initTimer + 0.05 * Random.Range(0, 5);  
+    }
+}
